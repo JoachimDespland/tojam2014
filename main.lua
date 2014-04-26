31e4f9c563fff1766eaac4aa1ab1db2d174a02c1
JoachimDespland <joachimdespland@gmail.com>
2014-04-26 16:07:17 -0400

    graphics!

--- main.lua
+++ main.lua
@@ -1,10 +1,19 @@
 function love.load()
-	mode_success = love.window.setMode( 1280, 720, {vsync=true, fullscreen=false})
+	mode_success = love.window.setMode( 1280, 720, {vsync=true, fullscreen=true})
 
 	love.graphics.setDefaultFilter("nearest", "nearest", 0)
-	image = love.graphics.newImage("tiles.png")
+	image = love.graphics.newImage("puffin_sprite_sm.png")
+
+	bg0 = love.graphics.newImage("Background_Skyback.png")
+	print(bg0)
+	bg1 = love.graphics.newImage("Background_Clouds.png")
+	bg2 = love.graphics.newImage("Background_Clouds Front.png")
+	bg3 = love.graphics.newImage("Background_Cliffs Back.png")
+	bg4 = love.graphics.newImage("Background_Cliffs.png")
+	bg5 = love.graphics.newImage("Background_Water back.png")
+
 	fillSprites()
-	canvas = love.graphics.newCanvas(512, 512)
+	canvas = love.graphics.newCanvas(1024, 1024)
 
 	-- world variables
 	airdensity = 0.001225
@@ -13,8 +22,8 @@ function love.load()
 	waterline = 180
 
 	--puffin variables
-	flypower = 0.005
-	swimpower = 0.005
+	flypower = 0.25
+	swimpower = 0.25
 	swimspeed = 100
 	flyspeed = 100
 	swimlift = 0.0002
@@ -24,28 +33,56 @@ function love.load()
 	physics = {}
 	draw = {}
 	puffins = {}
+	animations = {}
 
 	-- create a puffin and add it to component tables
 	puffin = {
+		--sprite
+		sprite = 0, 
+		line = 0,
+
+		--physics
 		px = 160, 
 		py = 0, 
 		vx = 0, 
 		vy = 0, 
 		mass = 0.5, 
 		density = 0.6, 
-		sprite = 1, 
 		drag = 0.001,
+
+		--puffin
+		flaptimer = 0,
+		flapkey = false,
+		flapnext = false,
 		movex = 0,
-		movey = 0
+		movey = 0,
+
+		--animation
+		animstate = 0,
+		animspeed = 10,
+		animstart = 0,
+		animcount = 4,
+		animloop = true
 	}	
 	table.insert(draw, puffin)
 	table.insert(physics, puffin)
 	table.insert(puffins, puffin)
+	table.insert(animations, puffin)
 
 	-- create a test object and add it to component tables
-	object = {px = 260, py = 190, vx = 0, vy = 0, mass = 0.5, density = 600, sprite = 2, drag = 0.1}	
+	object = {
+		sprite = 0,
+		line = 3, 
+		px = 260, 
+		py = 0, 
+		vx = 0, 
+		vy = 0, 
+		mass = 0.5, 
+		density = 0.6, 
+		drag = 0.001,
+	}	
 	table.insert(draw, object)
-	--table.insert(physics, object)
+	table.insert(physics, object)
 end
 
 function fillSprites()
@@ -53,27 +90,55 @@ function fillSprites()
 	local py = image:getWidth()
 
 	sprites = {
-	love.graphics.newQuad(64,0,16,16,px,py),
-	love.graphics.newQuad(64+16,0,16,16,px,py),
-	love.graphics.newQuad(64+32,0,16,16,px,py),
-	love.graphics.newQuad(64+48,0,16,16,px,py)}
+		love.graphics.newQuad(64,0,16,16,px,py),
+		love.graphics.newQuad(64+16,0,16,16,px,py),
+		love.graphics.newQuad(64+32,0,16,16,px,py),
+		love.graphics.newQuad(64+48,0,16,16,px,py)
+	}
 end
 
 function love.update(dt)
 	doPhysics(dt)
-	doPuffins()
+	doPuffins(dt)
+	doAnimations(dt)
 end
 
 function drawSprites()
+	love.graphics.draw(bg0,0,0)
+	love.graphics.draw(bg1,0,0)
+	love.graphics.draw(bg2,0,0)
+	love.graphics.draw(bg3,0,0)
+	love.graphics.draw(bg4,0,0)
+	
 	for k,v in pairs(draw) do
-		love.graphics.draw(image, sprites[v.sprite],v.px,v.py)
+		love.graphics.draw(
+			image, 
+			love.graphics.newQuad(v.sprite*16,v.line*16, 16,16, 128,384),
+			v.px-8,v.py-8
+		)
 	end
+	love.graphics.draw(bg5,0,0)	
 end
 
 function love.draw()
-	canvas:renderTo(love.graphics.clear)
 	canvas:renderTo(drawSprites)
+
 	love.graphics.draw(canvas,0,0,0,2)
+end
+
+function doAnimations(dt)
+	for k,v in pairs(animations) do
+		v.animstate = v.animstate + dt*v.animspeed
+		v.sprite = v.animstart+math.floor(v.animstate)
+		if v.sprite >= v.animstart+v.animcount then
+			if v.animloop then
+				v.sprite = v.sprite - v.animcount
+				v.animstate = v.animstate - v.animcount
+			else
+				v.sprite = v.animstart + v.animcount-1
+			end
+		end
+	end
 end
 
 function doPhysics(dt)
@@ -90,63 +155,19 @@ function doPhysics(dt)
 		local movelift = 0
 		if (v.py < waterline) then 
 			fluiddensity = airdensity
-			movepower = flypower
-			movespeed = flyspeed
-			movelift = flylift
 		else 
 			fluiddensity = waterdensity
-			movepower = swimpower
-			movespeed = swimspeed
-			movelift = swimlift
 		end
 
 		--buoyancy
 		local volume = v.mass / v.density
 		v.ay = v.ay - (fluiddensity*volume*gravity)/v.mass
 
-
-		local velocityx = v.vx
-		local velocityy = v.vy
-
-		--lift
-		local speed = math.sqrt(velocityx*velocityx+velocityy*velocityy)
-		local side = velocityy * v.movex - velocityx * v.movey
-		local dotproduct = (velocityx/speed) * v.movex + (velocityy/speed) * v.movey
-		if side ~= 0 and dotproduct > -0.3 then
-			local theta = -speed*movelift*(side/math.abs(side))
-			local cs = math.cos(theta);
-			local sn = math.sin(theta);
-
-			v.vx = velocityx * cs - velocityy * sn;
-			v.vy = velocityx * sn + velocityy * cs;
-		end
-
 		--drag
 		local drag = (fluiddensity*v.drag)/v.mass
 
 		v.vx = v.vx/(1+drag)
 		v.vy = v.vy/(1+drag)
-
-		--fly/swim
-		if v.movex ~= 0 or v.movey ~= 0 then
-
-			--propelling
-			local relvx = velocityx - movespeed*v.movex
-			local relvy = velocityy - movespeed*v.movey
-
-			local scalar = (relvx * v.movex + relvy * v.movey)
-
-			if scalar < 0 then
-				local projx = scalar*v.movex
-				local projy = scalar*v.movey
-
-				local rejx = v.movex - projx
-				local rejy = v.movey - projy
-
-				v.vx = v.vx + projx/(1+movepower) - projx
-				v.vy = v.vy + projy/(1+movepower) - projy
-			end
-		end
 
 		--velocity
 		v.vx = v.vx+v.ax*dt
@@ -158,7 +179,7 @@ function doPhysics(dt)
 	end
 end
 
-function doPuffins()
+function doPuffins(dt)
 	for k,v in pairs(puffins) do
 
 		if love.keyboard.isDown( "down" ) then
@@ -179,6 +200,67 @@ function doPuffins()
 		if (norm ~= 0) then
 			v.movex = v.movex/norm
 			v.movey = v.movey/norm
+		end
+
+		--------------------------
+		local velocityx = v.vx
+		local velocityy = v.vy
+
+		if (v.py < waterline) then 
+			movelift = flylift
+		else 
+			movelift = swimlift
+		end		
+
+		--lift
+		local speed = math.sqrt(velocityx*velocityx+velocityy*velocityy)
+		local side = velocityy * v.movex - velocityx * v.movey
+		local dotproduct = (velocityx/speed) * v.movex + (velocityy/speed) * v.movey
+		if side ~= 0 and dotproduct > -0.3 then
+			local theta = -speed*movelift*(side/math.abs(side))
+			local cs = math.cos(theta);
+			local sn = math.sin(theta);
+
+			v.vx = velocityx * cs - velocityy * sn;
+			v.vy = velocityx * sn + velocityy * cs;
+		end	
+
+		--fly/swim-------------
+		if love.keyboard.isDown( "z" ) then
+			if v.flapkey == false and v.flaptimer < 0.2 then
+				v.flapnext = true
+			end
+			v.flapkey = true
+		else
+			v.flapkey = false
+		end
+
+		if v.flaptimer > 0 then
+			v.flaptimer = v.flaptimer - dt
+		end
+
+		if (v.py < waterline) then
+			if v.flapnext and v.flaptimer <= 0 then
+				v.flaptimer = 0.5
+				print "flap"
+				v.flapnext = false
+
+				v.vy = v.vy + (-flyspeed-v.vy)*flypower
+			end
+		else
+			if v.flapnext and v.flaptimer <= 0 then
+				v.flaptimer = 0.5
+				print "swim"
+				v.flapnext = false
+
+				if v.movex == 0 and v.movey == 0 then
+					v.movex = v.vx/speed
+					v.movey = v.vy/speed
+				end
+
+				v.vx = v.vx + (v.movex*swimspeed-v.vx)*swimpower*math.abs(v.movex)
+				v.vy = v.vy + (v.movey*swimspeed-v.vy)*swimpower*math.abs(v.movey)
+			end
 		end
 	end
 end

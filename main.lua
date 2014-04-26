5e833ec88f7d54512a49b4bf9c2c0646d8cc4c00
JoachimDespland <joachimdespland@gmail.com>
2014-04-25 20:16:26 -0400

    resolution fix and other things

--- main.lua
+++ main.lua
@@ -1,16 +1,5 @@
-function fillSprites()
-	local px = image:getHeight()
-	local py = image:getWidth()
-
-	sprites = {
-	love.graphics.newQuad(64,0,16,16,px,py),
-	love.graphics.newQuad(64+16,0,16,16,px,py),
-	love.graphics.newQuad(64+32,0,16,16,px,py),
-	love.graphics.newQuad(64+48,0,16,16,px,py)}
-end
-
 function love.load()
-	mode_success = love.window.setMode( 1920, 1080, {vsync=true, fullscreen=true})
+	mode_success = love.window.setMode( 1280, 720, {vsync=true, fullscreen=true})
 
 	love.graphics.setDefaultFilter("nearest", "nearest", 0)
 	image = love.graphics.newImage("tiles.png")
@@ -20,25 +9,54 @@ function love.load()
 	-- world variables
 	airdensity = 1.225
 	waterdensity = 1000
+	airdrag = 1
+	waterdrag = 10
 	gravity = 50
 	waterline = 180
 
-	-- game component tables
+	--puffin variables
+	airspower = 1
+	swimpower = 1
+	swimspeed = 1
+	airspeed = 1
+
+	-- variables component tables
 	physics = {}
 	draw = {}
 	puffins = {}
 
 	-- create a puffin and add it to component tables
-	puffin = {px = 160, py = 160, vx = 0, vy = 0, ax = 0, ay = 0, mass = 0.5, density = 600, sprite = 1}	
+	puffin = {
+		px = 160, 
+		py = 160, 
+		vx = 0, 
+		vy = 0, 
+		mass = 0.5, 
+		density = 600, 
+		sprite = 1, 
+		drag = 0.0001,
+		movex = 1,
+		movey = 0
+	}	
 	table.insert(draw, puffin)
 	table.insert(physics, puffin)
 	table.insert(puffins, puffin)
 
 	-- create a test object and add it to component tables
-	object = {px = 260, py = 190, vx = 0, vy = 0, ax = 0, ay = 0, mass = 0.5, density = 600, sprite = 2}	
+	object = {px = 260, py = 190, vx = 0, vy = 0, mass = 0.5, density = 600, sprite = 2, drag = 0.00001}	
 	table.insert(draw, object)
 	table.insert(physics, object)
+end
 
+function fillSprites()
+	local px = image:getHeight()
+	local py = image:getWidth()
+
+	sprites = {
+	love.graphics.newQuad(64,0,16,16,px,py),
+	love.graphics.newQuad(64+16,0,16,16,px,py),
+	love.graphics.newQuad(64+32,0,16,16,px,py),
+	love.graphics.newQuad(64+48,0,16,16,px,py)}
 end
 
 function love.update(dt)
@@ -55,11 +73,36 @@ end
 function love.draw()
 	canvas:renderTo(love.graphics.clear)
 	canvas:renderTo(drawSprites)
-	love.graphics.draw(canvas,0,0,0,3)
+	love.graphics.draw(canvas,0,0,0,2)
 end
 
 function doPhysics(dt)
 	for k,v in pairs(physics) do
+
+		--acceleration
+		v.ax = 0
+		v.ay = gravity
+
+		--fluid density
+		local fluiddensity = 0
+		if (v.py < waterline) then 
+			fluiddensity = airdensity
+		else 
+			fluiddensity = waterdensity
+		end
+
+		--buoyancy
+		local volume = v.mass / v.density
+		v.ay = v.ay - (fluiddensity*volume*gravity)/v.mass
+
+
+		--drag
+		local speed = math.sqrt(v.vx*v.vx+v.vy*v.vy)
+		local drag = (fluiddensity*speed*speed*v.drag)/v.mass
+		if (speed ~= 0) then
+			v.ax = v.ax - (v.vx/speed)*drag
+			v.ay = v.ay - (v.vy/speed)*drag
+		end
 
 		--velocity
 		v.vx = v.vx+v.ax*dt
@@ -68,14 +111,6 @@ function doPhysics(dt)
 		--position
 		v.px = v.px+v.vx*dt
 		v.py = v.py+v.vy*dt
-
-		--new acceleration
-		v.ax = 0
-		v.ay = gravity
-		local volume = v.mass / v.density
-		if (v.py < waterline) then v.ay = v.ay - (airdensity*volume*gravity)/v.mass
-		else v.ay = v.ay - (waterdensity*volume*gravity)/v.mass
-		end
 	end
 end
 
